80d9230ed214fa266fbc0db91575083dcc456a4a
Alexresh <alexresh62@yandex.ru>
2020-09-14 18:55:02 +0300

    Renamed nonAggressiveMob to MobManager Moved method from ShulkerRespawn to MobManager Deleted ShulkerRespawn Added charged creeper spawn

--- src/alexresh/vanilla/Main.java
+++ src/alexresh/vanilla/Main.java
@@ -1,5 +1,10 @@
 package alexresh.vanilla;
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
 import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.ShapelessRecipe;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin  {
@@ -8,10 +13,15 @@ public class Main extends JavaPlugin  {
     public void onEnable() {
         saveDefaultConfig();
         FileConfiguration config = getConfig();
-        getServer().getPluginManager().registerEvents(new ShulkerRespawn(config.getInt("shulkerRespawn.shulkerRespawnChance")), this);
+        if(config.getBoolean("bookWithoutLeather")) {
+            NamespacedKey key = new NamespacedKey(this, "book_without_leather");
+            ShapelessRecipe recipe = new ShapelessRecipe(key, new ItemStack(Material.BOOK));
+            recipe.addIngredient(5, Material.PAPER);
+            Bukkit.addRecipe(recipe);
+        }
         if(config.getBoolean("shulkerInShulker"))
             getServer().getPluginManager().registerEvents(new ShulkerInShulker(), this);
-        getServer().getPluginManager().registerEvents(new NonAggressiveMob(config), this);
+        getServer().getPluginManager().registerEvents(new MobManager(config), this);
         boolean craftingModule, enderChestModule;
         craftingModule = config.getBoolean("portableInventoryModule.portableCraftingTable");
         enderChestModule = config.getBoolean("portableInventoryModule.portableEnderChest");
--- src/alexresh/vanilla/MobManager.java
+++ src/alexresh/vanilla/MobManager.java
@@ -1,6 +1,8 @@
 package alexresh.vanilla;
 
+import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.World;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.*;
 import org.bukkit.event.EventHandler;
@@ -13,18 +15,18 @@ import org.bukkit.inventory.meta.SkullMeta;
 
 import java.util.Random;
 
-public class NonAggressiveMob implements Listener {
+public class MobManager implements Listener {
 
     private final FileConfiguration config;
 
-    NonAggressiveMob(FileConfiguration config){
+    MobManager(FileConfiguration config){
         this.config = config;
     }
 
 
     @EventHandler
     public void mobTargetPlayer(EntityTargetEvent event){
-        if(!config.getBoolean("nonAggressiveMobModule.mobsDoNotAttackThePlayerWithTheirHead"))
+        if(!config.getBoolean("mobModule.mobsDoNotAttackThePlayerWithTheirHead"))
             return;
         Entity mob = event.getEntity();
         Entity target = event.getTarget();
@@ -43,13 +45,43 @@ public class NonAggressiveMob implements Listener {
         }
     }
     @EventHandler
-    public void onBadMobSpawn(EntitySpawnEvent event){
-        if((event.getEntityType().equals(EntityType.BAT))&&(config.getBoolean("nonAggressiveMobModule.disableBatSpawn"))){
+    public void onMobSpawn(EntitySpawnEvent event){
+        EntityType entityType = event.getEntityType();
+        //disable bat spawn
+        if((entityType.equals(EntityType.BAT))&&(config.getBoolean("mobModule.disableBatSpawn"))){
             event.setCancelled(true);
             return;
         }
-        if((event.getEntityType().equals(EntityType.PHANTOM))&&(config.getBoolean("nonAggressiveMobModule.disablePhantomSpawn"))){
+        //disable phantom spawn
+        if((entityType.equals(EntityType.PHANTOM))&&(config.getBoolean("mobModule.disablePhantomSpawn"))){
             event.setCancelled(true);
+            return;
+        }
+        //charged creeper spawn
+        if(entityType.equals(EntityType.CREEPER)){
+            Random r = new Random();
+            int chance = config.getInt("mobModule.chargedCreeperSpawnChance");
+            int rand = r.nextInt(100)+1;
+            if(rand<=chance){
+                ((Creeper)event.getEntity()).setPowered(true);
+            }
+        }
+        //shulker respawn
+        if (entityType.equals(EntityType.ENDERMAN)){
+            Entity enderman = event.getEntity();
+            Random random = new Random();
+            if(enderman.getWorld().getEnvironment() == World.Environment.THE_END){
+                if(enderman.getLocation().subtract(0, 1, 0).getBlock().getType().toString().contains("PURPUR")||enderman.getLocation().getBlock().getType().toString().contains("PURPUR")){
+                    int randomPercent = random.nextInt(100)+1;//1-100
+                    int chance = config.getInt("shulkerRespawnChance");
+                    if(randomPercent<=chance){
+                        Location location = enderman.getLocation();
+                        World world = enderman.getWorld();
+                        event.setCancelled(true);
+                        world.spawn(location, Shulker.class);
+                    }
+                }
+            }
         }
     }
 
@@ -57,25 +89,25 @@ public class NonAggressiveMob implements Listener {
     public void mobDeath(EntityDeathEvent event){
         if(event.getEntityType() == EntityType.CREEPER){
             Random r = new Random();
-            if(r.nextInt(100)+1<=config.getInt("nonAggressiveMobModule.creeperDropHead.chance")){
+            if(r.nextInt(100)+1<=config.getInt("mobModule.creeperDropHead.chance")){
                 event.getDrops().add(new ItemStack(Material.CREEPER_HEAD));
             }
         }
         if(event.getEntityType() == EntityType.ZOMBIE){
             Random r = new Random();
-            if(r.nextInt(100)+1<=config.getInt("nonAggressiveMobModule.zombieDropHead.chance")){
+            if(r.nextInt(100)+1<=config.getInt("mobModule.zombieDropHead.chance")){
                 event.getDrops().add(new ItemStack(Material.ZOMBIE_HEAD));
             }
         }
         if(event.getEntityType() == EntityType.SKELETON){
             Random r = new Random();
-            if(r.nextInt(100)+1<config.getInt("nonAggressiveMobModule.skeletonDropHead.chance")){
+            if(r.nextInt(100)+1<config.getInt("mobModule.skeletonDropHead.chance")){
                 event.getDrops().add(new ItemStack(Material.SKELETON_SKULL));
             }
         }
         if(event.getEntityType() == EntityType.PLAYER){
             Random r = new Random();
-            if(r.nextInt(100)+1<config.getInt("nonAggressiveMobModule.playerDropHead.chance")){
+            if(r.nextInt(100)+1<config.getInt("mobModule.playerDropHead.chance")){
                 ItemStack playerHead = new ItemStack(Material.PLAYER_HEAD);
                 SkullMeta meta = (SkullMeta) playerHead.getItemMeta();
                 if(meta!=null){
